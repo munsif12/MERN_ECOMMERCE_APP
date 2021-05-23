96c64cc222daab226feee18688e9dc975790ce10
munisf12 <munsifalimisri69@gmail.com>
2021-05-23 20:26:54 +0500

    Products ja data for items available array of objects

---
+++ backend/data/products.js
@@ -0,0 +1,10 @@
+const products = [
+  {
+    name: "iPhone",
+    description: "this is my first ecommerce app",
+    imgUrl: "no photoyet",
+    price: 459,
+    countInStock: 8,
+  },
+];
+module.exports = products;

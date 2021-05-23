aa7bf3f4e604002096049223d9dddbdf7d67648e
munisf12 <munsifalimisri69@gmail.com>
2021-05-23 20:25:09 +0500

    includes the database file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,22 @@
+// require("dotenv").config(); agr ya  na b likho to chally ga bcoz main server wali file ma likh deya h
 const mongoose = require("mongoose");
 const connection = async () => {
   try {
-    await mongoose.connect(
-      process.env.DB_URL,
-      () => {
-        console.log(`Connection Sucessfull`);
-      },
-      {
+    await mongoose
+      .connect(process.env.DB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-      }
-    );
+      })
+      .then(() => console.log(`Connection to MongoDb Atlas Success`))
+      .catch((error) => {
+        console.error(`Connection to Db fail`);
+        console.error(error);
+        process.exit(1);
+      });
   } catch (error) {
-    console.log(`Connection to Db fail`);
-    console.log(error);
-    process.emit(1);
+    console.error(`Connection to Db fail`);
+    console.error(error);
+    process.exit(1);
   }
 };
 module.exports = connection;

1baa9c68453c10c553202cf38b803ef7697c393c
munsif12 <munsifalimisri69@gmail.com>
2021-05-25 23:12:54 +0500

    main server.js file backend work Almost done 95%

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,15 @@ require("dotenv").config(); //requiring dotenv file
 const app = express();
 const connection = require("./config/db");
 const PORT = process.env.PORT || 8000;
+const productRoutes = require("./routes/productRoutes");
 
 //connection to database
 connection();
 //middlewares
-app.get("/", (req, res) => {
-  res.json({ message: "server Success" });
-});
+app.use(express.json());
 //routes
+app.use("/api/products", productRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });

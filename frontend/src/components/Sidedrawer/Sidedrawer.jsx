61540327df0a890e87c5ddc84519ed57a53e28ce
munsif12 <munsifalimisri69@gmail.com>
2021-05-25 23:09:53 +0500

    responsive sidedrawer work done

--- frontend/src/components/Sidedrawer/Sidedrawer.jsx
+++ frontend/src/components/Sidedrawer/Sidedrawer.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./sidedrawer.css";
-function Sidedrawer({ show }) {
+function Sidedrawer({ show, click }) {
   const sideDrawerClass = ["sidedrawer"];
   if (show) {
     sideDrawerClass.push("show");
   }
-  return <div className={sideDrawerClass.join(" ")}></div>;
+  return (
+    <div className={sideDrawerClass.join(" ")}>
+      <ul className="sidedrawer__links" onClick={click}>
+        <li>
+          <NavLink exact to="/cart">
+            <i className="fas fa-shopping-cart"></i>
+            <span>
+              Cart
+              <span className="sidedrawer__cartItemCounter">0</span>
+            </span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/">
+            Shop
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Sidedrawer;

0f20cd292975387686a5bbd8738e08e32d90b8d7
munisf12 <munsifalimisri69@gmail.com>
2021-05-27 14:12:43 +0500

    HomeScreen||ProductsScreen which will show all the products available

---
+++ frontend/src/Screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import "./HomeScreen.css";
+import Product from "./Product/Product";
+import { useSelector, useDispatch } from "react-redux";
+import { getProducts as listProducts } from "../../redux/actions/productActions";
+function HomeScreen() {
+  const dispatch = useDispatch();
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products, loading, error } = getProducts;
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+  return (
+    <div className="homeScreen">
+      <h1 className="homeScreen__heading">Products</h1>
+      <div className="homeScreen__products">
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : error ? (
+          <h1>{error}</h1>
+        ) : (
+          products.map((prod) => (
+            <Product
+              id={prod._id}
+              item={prod.name}
+              description={prod.description}
+              price={prod.price}
+              imgUrl={prod.imgUrl}
+            />
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default HomeScreen;

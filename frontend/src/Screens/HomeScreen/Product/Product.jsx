dd485cd21318e2cf237eda9feea3ab952c8e66ef
munisf12 <munsifalimisri69@gmail.com>
2021-05-27 14:11:12 +0500

    A single product means a single item on the products page

---
+++ frontend/src/Screens/HomeScreen/Product/Product.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Product.css";
+function Product({ id, item, description, price, imgUrl }) {
+  return (
+    <div className="product">
+      <img src={imgUrl} alt="" srcset="" />
+      <div className="product__info">
+        <p className="info__name">{item}</p>
+        <p className="info__desc">{description}</p>
+        <p className="info__price">{price}</p>
+        <NavLink exact to={`/product/${id}`} className="info__viewProduct">
+          View Product
+        </NavLink>
+      </div>
+    </div>
+  );
+}
+
+export default Product;

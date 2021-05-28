64409321bf514916efa889bd3c35e45d303bdc1a
munisf12 <munsifalimisri69@gmail.com>
2021-05-28 17:32:25 +0500

    SideDrawer done

--- frontend/src/components/Sidedrawer/Sidedrawer.jsx
+++ frontend/src/components/Sidedrawer/Sidedrawer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./sidedrawer.css";
+
+import { useSelector } from "react-redux";
 function Sidedrawer({ show, click }) {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  function cartItemsCount() {
+    return cartItems.reduce((qty, prod) => qty + Number(prod.qty), 0);
+  }
   const sideDrawerClass = ["sidedrawer"];
   if (show) {
     sideDrawerClass.push("show");
@@ -14,7 +21,9 @@ function Sidedrawer({ show, click }) {
             <i className="fas fa-shopping-cart"></i>
             <span>
               Cart
-              <span className="sidedrawer__cartItemCounter">0</span>
+              <span className="sidedrawer__cartItemCounter">
+                {cartItemsCount()}
+              </span>
             </span>
           </NavLink>
         </li>

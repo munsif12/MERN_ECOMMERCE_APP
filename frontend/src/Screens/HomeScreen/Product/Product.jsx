4d0cd25c8b4dfddd48552c287c974506c12bc1ac
munisf12 <munsifalimisri69@gmail.com>
2021-05-30 21:00:40 +0500

    Home screen contains all the available products

--- frontend/src/Screens/HomeScreen/Product/Product.jsx
+++ frontend/src/Screens/HomeScreen/Product/Product.jsx
@@ -4,11 +4,13 @@ import "./Product.css";
 function Product({ id, item, description, price, imgUrl }) {
   return (
     <div className="product">
-      <img src={imgUrl} alt={item} srcset="" />
+      <div className="imgCont">
+        <img src={imgUrl} alt={item} srcset="" />
+      </div>
       <div className="product__info">
         <p className="info__name">{item}</p>
         <p className="info__desc">{description.substring(0, 100)}</p>
-        <p className="info__price">{price}</p>
+        <p className="info__price">{price} Rs.</p>
         <NavLink exact to={`/product/${id}`} className="info__viewProduct">
           View Product
         </NavLink>

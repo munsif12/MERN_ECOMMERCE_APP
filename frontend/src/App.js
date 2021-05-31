8ed2b783f3662d4afaf506785e61e08b27bd6be9
munisf12 <munsifalimisri69@gmail.com>
2021-05-31 20:24:22 +0500

    main app.js file of ecommerce-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+
+//importing screens
+import HomeScreen from "./Screens/HomeScreen/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen/ProductScreen";
+import CartScreen from "./Screens/CartScreen/CartScreen";
+import Sidedrawer from "./components/Sidedrawer/Sidedrawer";
+// components
+import Navbar from "./components/Navbar/Navbar";
+import Backdrop from "./components/Backdrop/Backdrop";
+import Footer from "./components/Footer/Footer";
 
 function App() {
+  const [sideMenuToggle, setSideMenuToggle] = useState(false);
+  // console.log(sideMenuToggle); testing
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Navbar */}
+      <Navbar click={() => setSideMenuToggle(true)} />
+      {/* Sidedrawer */}
+      <Sidedrawer
+        show={sideMenuToggle}
+        click={() => setSideMenuToggle(false)}
+      />
+      {/* Backdrop */}
+      <Backdrop show={sideMenuToggle} click={() => setSideMenuToggle(false)} />
+
+      <main>
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart" component={CartScreen} />
+        </Switch>
+      </main>
+      <Footer />
+      {/*  */}
     </div>
   );
 }

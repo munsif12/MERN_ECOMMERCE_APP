5f502ae77dbf0b9118b185704e9b369a9e8454c5
munisf12 <munsifalimisri69@gmail.com>
2021-05-31 20:23:03 +0500

    added Loader for the ecommerce-app

---
+++ frontend/src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Loading.css";
+function Loading() {
+  return (
+    <div className="Loading">
+      <div class="lds-hourglass"></div>
+    </div>
+  );
+}
+
+export default Loading;

b04a2dab36dcfdc5bb95009b1e158491ec6e1f66
munisf12 <munsifalimisri69@gmail.com>
2021-05-31 20:22:41 +0500

    added footer for the ecommerce-app

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import "./Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <h2>This is footer</h2>
+      <div className="F__left">
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-linkedin"></i>
+        <i class="fab fa-facebook"></i>
+      </div>
+      <div className="F__right">
+        <h3> &copy; Munsif Ali Misri {new Date().getFullYear()}</h3>
+      </div>
     </div>
   );
 }

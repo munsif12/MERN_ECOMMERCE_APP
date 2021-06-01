09c2533bc3ebe293a2427db95a54eeb0215091e1
munisf12 <munsifalimisri69@gmail.com>
2021-06-02 01:18:43 +0200

    Main server.js file for backend

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const connection = require("./config/db");
 const PORT = process.env.PORT || 8000;
 const productRoutes = require("./routes/productRoutes");
 
+//Cors origin policy
+var cors = require("cors");
+app.use(cors());
+//if proxy in front end not working the you can use cors pakage to avoid cross origin policy
 //connection to database
 connection();
 //middlewares

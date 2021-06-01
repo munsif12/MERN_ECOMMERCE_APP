ceba8319efede1c7029f169df98d57055b4ad103
munisf12 <munsifalimisri69@gmail.com>
2021-06-01 15:32:09 +0500

    will show the details of a single product

--- frontend/src/Screens/ProductScreen/ProductScreen.jsx
+++ frontend/src/Screens/ProductScreen/ProductScreen.jsx
@@ -2,14 +2,20 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./ProductScreen.css";
 
+//reacr-tostify for notifiacation
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 //actions
 import { getProductDetails as getProductDetailsAction } from "../../redux/actions/productActions";
 import { addToCart } from "../../redux/actions/CartActions";
+import Loading from "../../components/Loading/Loading";
 function ProductScreen({ match, history }) {
   const [Qty, setQty] = useState(1); // quantity of available items in the stock for select option
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.getProductDetails);
+  const checkForItemExistance = useSelector((state) => state.cart);
   const { product, loading, error } = productDetails;
+  const { cartItems } = checkForItemExistance;
   useEffect(() => {
     // dispatch(getProductDetailsAction(match.params.id));
     if (product && match.params.id !== product._id) {
@@ -17,13 +23,28 @@ function ProductScreen({ match, history }) {
     }
   }, [dispatch, product, match]);
   function addToCartItem() {
-    dispatch(addToCart(product._id, Qty));
-    history.push("/cart");
+    //check if item already exist in the stock
+    const ifItemExists = cartItems.some((prod) => product._id === prod.product);
+    // console.log(ifItemExists);
+    if (!ifItemExists) {
+      dispatch(addToCart(product._id, Qty));
+      history.push("/cart");
+    } else {
+      toast.success("Item already present in the cart!", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
   }
   return (
     <div className="productScreen">
       {loading ? (
-        <h2>Loading...</h2>
+        <Loading />
       ) : error ? (
         <h2>{error}</h2>
       ) : (
@@ -31,28 +52,27 @@ function ProductScreen({ match, history }) {
           <div className="productScreen__left">
             <div className="left__image">
               <img src={product.imgUrl} alt="" srcset="" />
-              {console.log(product.imgUrl)}
+              {/* {console.log(product.imgUrl)} testing*/}
             </div>
             <div className="left__info">
               <p className="left__name">{product.name}</p>
-              <p className="left__price">{product.price}</p>
+              <p className="left__price">Price : {product.price} Rs</p>
               <p className="left__desc">{product.description}</p>
             </div>
           </div>
           <div className="productScreen__right">
             <div className="right__info">
               <p>
-                Price:
-                <span>{product.price}</span>
+                Price :<span>{product.price} Rs</span>
               </p>
               <p>
-                Status:
+                Status :
                 <span>
                   {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
                 </span>
               </p>
               <p>
-                Qty:
+                Quantity :
                 <select value={Qty} onChange={(e) => setQty(e.target.value)}>
                   {/* //bit confued loking forward to implement */}
                   {[...Array(product.countInStock).keys()].map((val) => (
@@ -64,13 +84,14 @@ function ProductScreen({ match, history }) {
               </p>
               <p>
                 <button type="button" onClick={addToCartItem}>
-                  Add product
+                  Add To Cart
                 </button>
               </p>
             </div>
           </div>
         </>
       )}
+      <ToastContainer />
     </div>
   );
 }
